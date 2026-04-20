6d86dc229095972c0c65dc08dc3be7c50995c3b8
StefanL02 <113446954+StefanL02@users.noreply.github.com>
2026-04-20 23:14:29 +0100

    Add Terraform main.tf
    Signed-off-by: StefanL02 <113446954+StefanL02@users.noreply.github.com>

---
+++ terraform/main.tf
@@ -0,0 +1,55 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_security_group" "petclinic_sg" {
+  name        = "petclinic-sg"
+  description = "Security group for PetClinic app"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "SSH access"
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "App access"
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "petclinic-sg"
+  }
+}
+
+resource "aws_instance" "petclinic" {
+  ami                    = "ami-0905a3c97561e0b69"
+  instance_type          = "t2.micro"
+  key_name               = var.key_name
+  vpc_security_group_ids = [aws_security_group.petclinic_sg.id]
+
+  tags = {
+    Name = "petclinic-server"
+  }
+}
